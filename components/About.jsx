f2cbf276ea8285c121e6f4c46373c27b9b58771f
Andy Pham <andypham1303@gmail.com>
2025-06-25 04:17:54 +1000

    About page setup

---
+++ components/About.jsx
@@ -0,0 +1,19 @@
+import DevImg from './DevImg'
+import Image from 'next/image'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+
+import { User2, MailIcon, HomeIcon, PhoneCall, GraduationCap, Calendar, Briefcase, } from 'lucide-react'
+
+const infoData = [
+    {
+        icon: <User2 size={20} />,
+        text: 'Ryan Davis',
+    },
+
+]
+
+const About = () => {
+    return <div>About</div>
+}
+
+export default About
