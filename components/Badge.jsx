97978cddce33def221f6e5a87cbe88eb76a4d960
Andy Pham <andypham1303@gmail.com>
2025-06-25 02:58:36 +1000

    initial commit

---
+++ components/Badge.jsx
@@ -0,0 +1,12 @@
+'use cliunt';
+import React from 'react'
+import CountUp from 'react-countup'
+
+const Badge = () => {
+    return (
+        <div>Badge</div>
+
+    )
+}
+
+export default Badge
---
+++ components/Socials.jsx
@@ -0,0 +1,31 @@
+'use client'
+import { RiLinkedinFill, RiGithubFill } from 'react-icons/ri'
+
+import Link from 'next/link'
+
+const icons = [
+    {
+        path: '/',
+        name: <RiGithubFill />
+    },
+    {
+        path: '/',
+        name: <RiLinkedinFill />
+    }
+]
+
+const Socials = ({ containerStyles, iconsStyles }) => {
+    return (
+        <div className={`${containerStyles}`}>
+            {icons.map((icon, index) => {
+                return (
+                    <Link href={icon.path} key={index}>
+                        <div className={`${iconsStyles}`}>{icon.name}</div>
+                    </Link>
+                );
+            })}
+        </div>
+    )
+}
+
+export default Socials
