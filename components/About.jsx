219f05e0beebe0252812f8a583807c10cb7b2358
Andy Pham <andypham1303@gmail.com>
2025-06-25 19:50:17 +1000

    experience about me complete

--- components/About.jsx
+++ components/About.jsx
@@ -162,14 +162,60 @@ const About = () => {
                                         </div>
                                     </div>
                                 </TabsContent>
-                                <TabsContent value="qualifications">qualifications info</TabsContent>
+                                <TabsContent value="qualifications">
+                                    <div>
+                                        <h3 className='h3 mb-8 text-center xl:text-left'>
+                                            My Journey
+                                        </h3>
+                                        {/* exp & edu */}
+                                        <div>
+                                            {/* exp */}
+                                            <div>
+                                                <div className='flex gap-x-4 items-center text-[22px] text-primary'>
+                                                    <Briefcase />
+                                                    <h4 className='capitalize font-medium'>
+                                                        {getData(qualificationData, 'experience').title}
+                                                    </h4>
+                                                </div>
+                                                {/* list */}
+                                                <div className='flex flex-col gap-y-8'>
+                                                    {getData(qualificationData, 'experience').data.map((item, index) => {
+                                                        const { company, role, period } = item
+                                                        return (
+                                                            <div className='flex gap-x-8 group' key={index}>
+                                                                <div className='h-[84px] w-[1px] bg-border relative ml-2'>
+                                                                    <div className='h-[11px] w-[11px] rounded-full bg-primary absolute
+                                                                    -left-[5px] group-hover:translate-y-[84px] translate-all duration-500
+                                                                    '></div>
+                                                                </div>
+                                                                <div>
+                                                                    <div className='font-semibold text-xl leading-none mb-2'>
+                                                                        {company}
+                                                                    </div>
+                                                                    <div className='text-lg leading-none text-muted-foreground mb-4'>
+                                                                        {role}
+                                                                    </div>
+                                                                    <div className='text-base font-medium'>
+                                                                        {period}
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        )
+                                                    })}
+                                                </div>
+                                            </div>
+                                        </div>
+                                        {/* edu */}
+                                        <div>education</div>
+                                    </div>
+                                </TabsContent>
                                 <TabsContent value="skills">skills info</TabsContent>
                             </div>
                         </Tabs>
                     </div>
                 </div>
-            </div>
-        </section>
+            </div >
+        </section >
     );
 
 }
