ee00a0be39895605b0289b768cb9638e9080d797
Andy Pham <andypham1303@gmail.com>
2025-06-25 19:23:55 +1000

    about me personal information design complete

--- components/DevImg.jsx
+++ components/DevImg.jsx
@@ -3,7 +3,6 @@ import Image from "next/image"
 const DevImg = ({ containerStyles, imgSrc }) => {
     return (
         <div className={`${containerStyles}`}>
-            DevImg
             <Image src={imgSrc} fill priority alt='' />
         </div>
     )
