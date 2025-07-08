b3a09fd6ecc0a0bec1a0b23c8b876cf266cfa130
Andy Pham <andypham1303@gmail.com>
2025-07-08 22:25:14 +1000

    Interest ection complete

--- components/Interests.jsx
+++ components/Interests.jsx
@@ -1,4 +1,4 @@
-import { GanttChartSquare, Blocks, Gem, } from "lucide-react"
+import { GanttChartSquare, Blocks, Brush, Gamepad2, Container, Dock } from "lucide-react"
 import {
     Card,
     CardContent,
@@ -13,7 +13,32 @@ const interestsData = [
     {
         icon: <GanttChartSquare size={72} strokeWidth={0.8} />,
         title: 'Web Design',
-        description: ''
+        description: ' I’ve learned various design principles and elements, highlighting the importance of design to user’s experience.'
+    },
+    {
+        icon: <Blocks size={72} strokeWidth={0.8} />,
+        title: 'Web Development',
+        description: 'I have a passion for learning new frameworks and different techniques in software architecture.'
+    },
+    {
+        icon: <Dock size={72} strokeWidth={0.8} />,
+        title: 'Desktop App Development',
+        description: 'My first coding experiences involved developing desktop applications, introducing me to the ever expanding world of programming.'
+    },
+    {
+        icon: <Gamepad2 size={72} strokeWidth={0.8} />,
+        title: 'Game Development',
+        description: 'I have recently picked up Godot. Learning the limitations and usages of the game engine through developing small games.'
+    },
+    {
+        icon: <Brush size={72} strokeWidth={0.8} />,
+        title: 'Arts and crafts',
+        description: 'I like to use phyiscal and digital mediums to express creativity. I sell crochet plushies and I’m currently learning pixel art. '
+    },
+    {
+        icon: <Container size={72} strokeWidth={0.8} />,
+        title: 'DevOps',
+        description: 'The automation of software develeopment and delivery has a vast amount of options. The way different tools and services interact with one another facinates me.'
     },
 ]
 
@@ -25,7 +50,7 @@ export const Interests = () => {
                     My Interests
                 </h2>
                 {/* grid items */}
-                <div className="grid xl:grid-cols-3 justify-center gap-y-12 xl:gap-y-24">
+                <div className="grid xl:grid-cols-3 justify-center gap-y-12 xl:gap-y-24 xl:gap-x-8">
                     {
                         interestsData.map((item, index) => {
                             return (
