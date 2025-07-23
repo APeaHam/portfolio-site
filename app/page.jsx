fb941e2922efcae956c42ed7f7def48191f0667a
Andy Pham <andypham1303@gmail.com>
2025-07-23 19:37:31 +1000

    home page complete. needs info and photo update.

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import About from "@/components/About";
 import Hero from "@/components/Hero";
 import Interests from "@/components/Interests";
 import Work from "@/components/Work";
+import Cta from "@/components/Cta";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <About />
       <Interests />
       <Work />
+      <Cta />
     </main>
   )
 }
